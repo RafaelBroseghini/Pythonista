67be0156734ad8340fcec305e86c25270021975e
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2017-04-25 12:07:19 -0500

    Add files via upload

---
+++ Week10ExerciseBroseghini.py
@@ -0,0 +1,273 @@
+'''
+Name: Rafael Broseghini
+Prof: Roman Yasinovskyy
+
+Filename: Week10ExerciseBroseghini.py
+Date: 04/20/2017
+'''
+
+
+# Task 1
+def BinaryTreeLst(r):
+    return [r, [], []]
+
+def insertLeft(root,newBranch):
+    t = root.pop(1)
+    if len(t) > 1:
+        root.insert(1,[newBranch,t,[]])
+    else:
+        root.insert(1,[newBranch, [], []])
+    return root
+
+def insertRight(root,newBranch):
+    t = root.pop(2)
+    if len(t) > 1:
+        root.insert(2,[newBranch,[],t])
+    else:
+        root.insert(2,[newBranch,[],[]])
+    return root
+
+def getRootVal(root):
+    return root[0]
+
+def setRootVal(root,newVal):
+    root[0] = newVal
+
+def getLeftChild(root):
+    return root[1]
+
+def getRightChild(root):
+    return root[2]
+
+def build_tree_lst():
+    r = BinaryTreeLst('a')
+    insertLeft(r,'b')
+    insertRight(r, 'c')
+    l = getLeftChild(r)
+    p = getRightChild(r)
+    insertRight(l,'d')
+    insertLeft(p, 'e')
+    insertRight(p, 'f')
+    print(r)
+
+def main():
+    print()
+    print('Task 1:')
+    print('Creating a tree that uses textbook implementation of a tree as a list of lists:')
+    build_tree_lst()
+    print()
+main()
+
+
+
+
+
+
+# Task 2
+class BinaryTree:
+    def __init__(self, rootObj):
+        self.key = rootObj
+        self.leftChild = None
+        self.rightChild = None
+    
+    def insertLeft(self,newNode):
+        if self.leftChild == None:
+            self.leftChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.leftChild = self.leftChild
+            self.leftChild = t
+
+    def insertRight(self,newNode):
+        if self.rightChild == None:
+            self.rightChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.rightChild = self.rightChild
+            self.rightChild = t
+
+
+    def getRightChild(self):
+        return self.rightChild
+
+    def getLeftChild(self):
+        return self.leftChild
+
+    def setRootVal(self,obj):
+        self.key = obj
+
+    def getRootVal(self):
+        return self.key
+    
+    # Task 3
+    def height(self):
+        if self.key == None:
+            return 0
+        return max(self.leftChild.height() if self.leftChild is not None else 0, self.rightChild.height() if self.rightChild is not None else 0) + 1    
+    
+    def __str__(self):
+        return str(self.key)
+
+def build_tree_oop():
+    print('Task 2:')
+    print('Creating a tree that uses textbook implementation of a tree as nodes and references:')
+    print('See build_tree_oop() function.') 
+    print()
+    r = BinaryTree('a')
+    r.insertLeft('b')
+    r.insertRight('c')
+    r.getRightChild().insertLeft('e')
+    r.getRightChild().insertRight('f')
+    r.getLeftChild().insertRight('d')
+    print('Task 3:')
+    print('The height of the Binary Tree built within build_tree_oop() is:', r.height())    
+build_tree_oop()
+
+class MaxHeap:
+    def __init__(self, n=0):
+        self._items = []
+        self._size = n
+        
+
+    def is_empty(self):
+        return self._items == []
+
+    def is_full(self):
+        return len(self._items) >= self._size
+
+    def __len__(self):
+        return len(self._items)
+
+    def __str__(self):
+        return str(self._items)
+
+    def insert(self, item):
+        self._items.append(item)
+        self.percUp(len(self._items)-1)
+
+    def percUp(self, current):
+        while (current - 1) // 2 >= 0:
+            parent = (current - 1) // 2
+            if self._items[current] < self._items[parent]:
+                self._items[current], self._items[parent] = self._items[parent], self._items[current]
+            else:
+                return
+            current = parent
+
+    def get_min_child(self, parent):
+        if 2 * parent + 2 > len(self._items) - 1:
+            return 2 * parent + 1
+        else:
+            if self._items[2 * parent + 1] > self._items[2 * parent + 2]:
+                return 2 * parent + 1
+            else:
+                return 2 * parent + 2    
+
+    def percDown(self, current):
+        while 2*current + 1 < len(self._items):
+            smaller_index = self.get_min_child(current)
+            if self._items[current] < self._items[smaller_index]:
+                self._items[current], self._items[smaller_index] = self._items[smaller_index], self._items[current]
+            else:
+                return
+            current = smaller_index
+
+    def delete(self):
+        self._items[0], self._items[len(self._items)-1] = self._items[len(self._items)-1], self._items[0]
+        res = self._items.pop()
+        self.percDown(0)
+        return res
+
+
+    def build_heap(self, lst):
+        self._items = lst[:]
+        current = len(self._items) // 2 - 1
+        while current >= 0:
+            self.percDown(current)
+            current = current - 1
+            
+            
+            
+            
+my_heap = MaxHeap()
+lst = [10, 24, 21, 300, 2, 77, 99, 1995, 235, 122, 413, 89, 9, 43, 872, 54, 93]
+my_heap.build_heap(lst)
+print()
+print('Task 4:')
+print('Max Heap implementation', my_heap)
+
+class MaxHeapLimitedSize:
+    def __init__(self, n=7, items= []):
+        self._items = []
+        self._size = n
+        
+
+    def is_empty(self):
+        return self._items == []
+
+    def is_full(self):
+        return len(self._items) >= self._size
+
+    def __len__(self):
+        return len(self._items)
+
+    def __str__(self):
+        return str(self._items)
+
+    def insert(self, item):
+        self._items.append(item)
+        self.percUp(len(self._items)-1)
+
+    def percUp(self, current):
+        while (current - 1) // 2 >= 0:
+            parent = (current - 1) // 2
+            if self._items[current] < self._items[parent]:
+                self._items[current], self._items[parent] = self._items[parent], self._items[current]
+            else:
+                return
+            current = parent
+
+    def get_min_child(self, parent):
+        if 2 * parent + 2 > len(self._items) - 1:
+            return 2 * parent + 1
+        else:
+            if self._items[2 * parent + 1] > self._items[2 * parent + 2]:
+                return 2 * parent + 1
+            else:
+                return 2 * parent + 2    
+
+    def percDown(self, current):
+        while 2*current + 1 < len(self._items):
+            smaller_index = self.get_min_child(current)
+            if self._items[current] < self._items[smaller_index]:
+                self._items[current], self._items[smaller_index] = self._items[smaller_index], self._items[current]
+            else:
+                return
+            current = smaller_index
+
+    def delete(self):
+        self._items[0], self._items[len(self._items)-1] = self._items[len(self._items)-1], self._items[0]
+        res = self._items.pop()
+        self.percDown(0)
+        return res
+
+
+    def build_heap(self, lst):
+        self._items = lst[:]
+        while len(self._items) > self._size:
+            self._items.remove(min(self._items))
+        current = len(self._items) // 2 - 1
+        while current >= 0:
+            self.percDown(current)
+            current = current - 1
+            
+def main():            
+    my_heap = MaxHeapLimitedSize()
+    lst = [10, 24, 21, 300, 2, 77, 99, 1995, 235, 122, 413, 89, 9, 43, 872, 54, 93]
+    my_heap.build_heap(lst)
+    print()
+    print('Task 5:')
+    print('Max Heap with limited size of 7:', my_heap)
+    #print('Largest item in Max Heap with limited size of 7:', my_heap.delete())
+    #print('New Max Heap after deleting largest value:', my_heap)
+main()
---
+++ Week9Exercise.py
@@ -0,0 +1,331 @@
+'''
+Name: Rafael Broseghini
+Prof: Roman Yasinovskyy
+
+Date: 04/08/2017
+Filename: Week9Exercise.py
+'''
+import time
+import random
+
+print('Perfoming a benchmark analysis of a "slow" and a "fast" sorting algorithm:')
+print()
+print('Slow algorithm: ')
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+mylist = [random.randint(1, 1000) for i in range(10000)]
+start = time.process_time()
+bubbleSort(mylist)
+end = time.process_time()
+print('My list sorted using the bubble sort method:')
+print(mylist)
+print('Time elapsed during the bubble sort method :', end - start, 'sec')
+print()
+print('Fast algorithm: Quick Sort ')
+def quick_Sort(alist):
+   quick_SortHelper(alist,0,len(alist)-1)
+
+def quick_SortHelper(alist,first,last):
+   if first<last:
+
+       splitpoint = Partition(alist,first,last)
+
+       quick_SortHelper(alist,first,splitpoint-1)
+       quick_SortHelper(alist,splitpoint+1,last)
+
+
+def Partition(alist,first,last):
+   pivotvalue = alist[first]
+
+   leftmark = first+1
+   rightmark = last
+
+   done = False
+   while not done:
+
+       while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+           leftmark = leftmark + 1
+
+       while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
+           rightmark = rightmark -1
+
+       if rightmark < leftmark:
+           done = True
+       else:
+           temp = alist[leftmark]
+           alist[leftmark] = alist[rightmark]
+           alist[rightmark] = temp
+
+   temp = alist[first]
+   alist[first] = alist[rightmark]
+   alist[rightmark] = temp
+
+
+   return rightmark
+alist = []
+for i in range(10000):
+    number = random.randint(1, 1000)
+    alist.append(number)
+start = time.process_time()
+quick_Sort(alist)
+end = time.process_time()
+print('My list using the quick sort:')
+print(alist)
+print('Time elapsed during the quick sort method :', end - start, 'sec')
+
+
+def bubble_Sort(mylist):
+    for number in range(len(mylist)-1,0,-1):
+        for i in range(number):
+            if mylist[i]>mylist[i+1]:
+                mylist[i], mylist[i+1] = mylist[i+1], mylist[i]
+                
+
+mylist = [random.randint(1, 1000) for i in range(10000)]
+start = time.process_time()
+bubble_Sort(mylist)
+end = time.process_time()
+print()
+print('My list sorted using the bubble sort method using simultaneous assignment:')
+print(mylist)
+print('Time elapsed during the bubble sort method using simultaneous assignment:', end - start, 'sec')
+print()
+
+
+
+
+
+
+def shellSort(alist):
+    sublistcount = len(alist)//3
+    while sublistcount > 0:
+
+      for startposition in range(sublistcount):
+        gapInsertionSort(alist,startposition,sublistcount)
+
+      print("After increments of size",sublistcount,
+                                   "The list is",alist)
+      sublistcount = sublistcount // 3
+      
+def shellsort(alist):
+    sublistcount = len(alist)//4
+    while sublistcount > 0:
+
+      for startposition in range(sublistcount):
+        gapInsertionSort(alist,startposition,sublistcount)
+
+      print("After increments of size",sublistcount,
+                                   "The list is",alist)
+
+      sublistcount = sublistcount // 4
+      
+def shell_Sort(alist):
+    sublistcount = len(alist)//5
+    while sublistcount > 0:
+
+      for startposition in range(sublistcount):
+        gapInsertionSort(alist,startposition,sublistcount)
+
+      print("After increments of size",sublistcount,
+                                   "The list is",alist)
+      sublistcount = sublistcount // 5
+
+def gapInsertionSort(alist,start,gap):
+    for i in range(start+gap,len(alist),gap):
+
+        currentvalue = alist[i]
+        position = i
+
+        while position>=gap and alist[position-gap]>currentvalue:
+            alist[position]=alist[position-gap]
+            position = position-gap
+
+        alist[position]=currentvalue
+
+alist = []
+mylist = []
+onelist = []
+for i in range(10000):
+    number = random.randint(1, 1000)
+    alist.append(number)
+    mylist.append(number)
+    onelist.append(number)
+print('Sorting using shell method subslists of count 3:')
+start = time.process_time()
+shellSort(alist)
+end = time.process_time()
+print('Time elapsed during the shell sort method (sublists of count 3):', end - start, 'sec')
+print()
+print()
+print('Sorting using shell method subslists of count 4:')
+start = time.process_time()
+shellsort(mylist)
+end = time.process_time()
+print('Time elapsed during the shell sort method (sublists of count 4):', end - start, 'sec')
+print()
+print()
+print('Sorting using shell method subslists of count 5:')
+start = time.process_time()
+shell_Sort(onelist)
+end = time.process_time()
+print('Time elapsed during the shell sort method (sublists of count 5):', end - start, 'sec')
+print()
+print('My list using the shell sort method (sublist = 3 elements):')
+print(alist)
+print()
+print('My list using the shell sort method (sublist = 4 elements):')
+print(mylist)
+print()
+print('My list using the shell sort method (sublist = 5 elements):')
+print(onelist)
+print()
+
+
+def mergeSort(alist):
+    print("Splitting ",alist)
+    lefthalf = []
+    righthalf = []
+    if len(alist)>1:
+        mid = len(alist)//2
+        for i in range(mid):
+            lefthalf.append(alist[i])
+        print(lefthalf)
+        
+        for i in range(mid, len(alist)):
+            righthalf.append(alist[i])
+        print(righthalf)
+            
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ",alist)
+
+alist = []
+for i in range(1000): # Merge sort for 10000 numbers omitts output but it is one of the fastest algorithms. I have changed the number of loops to not omitt output.
+    x = random.randint(1, 1000)
+    alist.append(x)
+start = time.process_time()
+mergeSort(alist) 
+end = time.process_time()
+print(alist)
+print('Time elapsed during the merge sort method:', end - start, 'sec')
+print()
+
+def swap(array,a,b):
+    array[a],array[b] = array[b],array[a]
+    
+def partition(array,start,end):
+    median = (end - 1 - start) % 2
+    median = median + start
+    left = start + 1
+    if (array[median] - array[end-1])*(array[start]-array[median]) >= 0:
+        swap(array,start,median)
+    elif (array[end - 1] - array[median]) * (array[start] - array[end - 1]) >=0:
+        swap(array,start,end - 1)
+    pivot = array[start]
+    for right in range(start,end):
+        if pivot > array[right]:
+            swap(array,left,right)
+            left = left + 1
+    swap(array,start,left-1)
+    return left-1
+
+def quickSortHelper(array,start,end):
+    if start < end:
+        splitPoint = partition(array,start,end)
+        quickSortHelper(array,start,splitPoint)
+        quickSortHelper(array,splitPoint+1,end)
+        
+def quickSort(array):
+    quickSortHelper(array,0,len(array))
+    
+
+array = []
+for i in range(10000):
+    number = random.randint(1, 1000)
+    array.append(number)
+start = time.process_time()
+quickSort(array)
+end = time.process_time()
+print('My list using the quick sort median-of-three method:')
+print(array)
+print('Time elapsed during the quick sort median-of-three method:', end - start, 'sec')
+
+def quick_Sort(alist):
+   quick_SortHelper(alist,0,len(alist)-1)
+
+def quick_SortHelper(alist,first,last):
+   if first<last:
+
+       splitpoint = Partition(alist,first,last)
+
+       quick_SortHelper(alist,first,splitpoint-1)
+       quick_SortHelper(alist,splitpoint+1,last)
+
+
+def Partition(alist,first,last):
+   pivotvalue = alist[first]
+
+   leftmark = first+1
+   rightmark = last
+
+   done = False
+   while not done:
+
+       while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+           leftmark = leftmark + 1
+
+       while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
+           rightmark = rightmark -1
+
+       if rightmark < leftmark:
+           done = True
+       else:
+           temp = alist[leftmark]
+           alist[leftmark] = alist[rightmark]
+           alist[rightmark] = temp
+
+   temp = alist[first]
+   alist[first] = alist[rightmark]
+   alist[rightmark] = temp
+
+
+   return rightmark
+print()
+alist = []
+for i in range(10000):
+    number = random.randint(1, 1000)
+    alist.append(number)
+start = time.process_time()
+quick_Sort(alist)
+end = time.process_time()
+print('My list using the quick sort:')
+print(alist)
+print('Time elapsed during the quick sort method :', end - start, 'sec')
