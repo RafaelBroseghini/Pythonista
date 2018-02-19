a78f38e122d9dac064e837899a75ded8aefb09da
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-02-19 15:17:53 -0600

    Update MyPayroll.py

--- MyPayroll.py
+++ MyPayroll.py
@@ -7,7 +7,6 @@ Date: 02/12/2016
 
 Filename: BroseghiniPayroll.py
 '''
-# Specify all the input.
 
 name = input("Enter employee's name: ")
 
@@ -15,62 +14,46 @@ hrsWorked = float(input('Enter number of hours worked in a week: '))
 
 payRate = float(input('Enter hourly pay rate: '))
 
-#Formula for Gross Pay.
-
 grossPay = hrsWorked*payRate
 
-# Individual's input has to be divided by 100. Remember it is in percentage notation.
 
 federalTaxRate = float(input('Enter federal tax withholding rate: '))/100
 
-#Formula for Federal Deduction.
 
 federalDeduction = grossPay*federalTaxRate
 
-# Individual's input has to be divided by 100. Remember it is in percentage notation.
 
 stateTaxRate = float(input('Enter state tax withholding rate: '))/100
 
-#Formula for State Deduction.
 
 stateDeduction = grossPay*stateTaxRate
 
-#Look up on the Word document what is the medicare rate provided by Dr. Hardy.
 
 medicareRate = 1.45/100
 
-#Formula for medicare Deduction
 
 medicareDeduction = grossPay*medicareRate
 
-#Look up on the Word Document what is the social Security rate provided by Dr. Hardy.
 
 socialSecurityRate = 6.2/100
 
-#Formula for social Security deduction.
 
 socialSecurityDeduction = grossPay*socialSecurityRate
 
-#Formula for Total Deductions.
 
 totalDeductions = (federalDeduction + stateDeduction + medicareDeduction + socialSecurityDeduction)
 
-#Formula for Net Pay.
 
 netPay = (grossPay - federalDeduction - stateDeduction - medicareDeduction - socialSecurityDeduction)
 
-# Stethic spacing below for better visualizing.
 
 print()
 print()
 print()
 print()
 
-# Look up the WordDocument and check the data order.
 
 print('Employee Name:', name)
-
-# REMEMBER TO ROUND HOURS WORKED TO ONE DECIMAL AND MONETARY VALUES TO TWO DECIMALS.
 
 print('Hours Worked:', round(hrsWorked, 1))
 
@@ -79,8 +62,6 @@ print('Pay Rate: $', round(payRate, 2))
 print('Gross Pay: $', round(grossPay, 2))
 
 print('Deductions:')
-
-# Figure out a way to add margin!!!
 
 print('     ''Federal: $', round(federalDeduction, 2))
 
