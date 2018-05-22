0b81de53720bb9d71959f33fb31dd2ca5985d74e
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-05-21 23:16:54 -0500

    Colorization

--- WebScraper/lutherSports.py
+++ WebScraper/lutherSports.py
@@ -22,21 +22,20 @@ def print_sports(sex,lst):
     print("{:^70}".format(sex))
     print("-"*70+"\n")
     for elem in lst:
-        print("{:^70}\n".format(elem.text))
+        print("\033[1;31m{:^70}\033[0m\n".format(elem.text))
 
 def choose_sex():
-    print("\nHey there! Glad to have you here! Let's find out Sports for a chosen sex @ Luther College!\n")
-    print("Option 1: Men's")
-    print("Option 2: Women's\n")
+    print("\n\033[1;96mHey there! Glad to have you here! Let's find out Sports for a chosen sex @ Luther College!\033[0m\n")
+    print("\033[1;31mOption 1: Men's")
+    print("Option 2: Women's\033[0m\n")
     selected = 0
-    found = False
-    while not found:
-        selected = int(input("Choose 1 or 2 to see their respective sports at Luther College: "))
+    while True:
+        selected = input("Choose 1 or 2 to see their respective sports at Luther College: ")
         print()
-        if selected == 1:
-            return "Men's sports at Luther College",selected
-        elif selected == 2:
-            return "Women's sports at Luther College", selected
+        if selected == "1":
+            return "\033[1;5mMen's\033[0m sports at Luther College", int(selected)
+        elif selected == "2":
+            return "\033[1;5mWomen's\033[0m sports at Luther College", int(selected)
         else:
             print("{} is not supported: Choose either 1 or 2".format(selected))
 
