f42f154e9f9ad45da5913db4d5cb6b64356bc3cc
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-05-24 23:14:30 -0500

    new fn

--- Regexer/regexBeginner.py
+++ Regexer/regexBeginner.py
@@ -14,7 +14,7 @@ def hexadecimal(word):
     print("Is hexadecimal? ", end="")
     return bool(match)
 
-def aFollowedByThreeB(word):
+def aFollowedByThreeBs(word):
     pattern = re.compile(r"[a|A][b|B]{3}")
     match = pattern.search(word)
     print("Input string: \033[1;32m{}\033[0m".format(word))
@@ -27,6 +27,13 @@ def substituteSpaceForUnderscore(word):
     print("Input string: \033[1;32m{}\033[0m".format(word))
     print("After Space2Underscore substitution => ", end="")
     return match
+
+def isValidFraction(fraction):
+    pattern = re.compile(r"^[1-9][0-9]*\/[1-9][0-9]*")
+    match = pattern.search(str(fraction))
+    print("Input Fraction: \033[1;32m{}\033[0m".format(fraction))
+    print("Valid fraction? ", end="")
+    return bool(match)
 
 
 def displayOutput():
@@ -50,6 +57,10 @@ def displayOutput():
     print("{}\n".format(substituteSpaceForUnderscore("I like to beep_bop")))
     print("{}\n".format(substituteSpaceForUnderscore("my name is John Doe")))
     print("{}\n".format(substituteSpaceForUnderscore("Ninja mode from now         on")))
+    print("="*30)
+    print("{}\n".format(isValidFraction("0/3")))
+    print("{}\n".format(isValidFraction("11/32")))
+    print("{}\n".format(isValidFraction("9/184")))
 
 def main():
     displayOutput()
