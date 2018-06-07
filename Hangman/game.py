7e370df648489299220c0dab186c2a4dcf42eacd
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-06-06 20:40:36 -0500

    difficulties added

--- Hangman/game.py
+++ Hangman/game.py
@@ -3,18 +3,39 @@ import random
 import string
 
 class Hangman():
-    def __init__(self, words=[], used_letters = [], tries=10):
+    def __init__(self, words=[], used_letters = [], tries=10, difficulty={"e":["easy",(3,4)],"i":["intermediate",(5,7)],"h":["hard",(8,12)]}):
         self.words = words
         self.tries = tries
         self.used_letters = used_letters
+        self.difficulty = difficulty
 
+    def display_difficulties(self):
+        for k,v in self.difficulty.items():
+            print("({}): {}".format(k,v[0]))
+
+    def display_intro_set_difficulty(self):
+        print("Hi there! Let's play hangman!")
+        print("-"*30+"\n")
+        print("First you need to select a difficulty: \n")
+
+        self.display_difficulties()
+        
+        diff = input("Difficulty: ")
+        while diff not in self.difficulty:
+            print("{} is not a(n) supported difficulty input. Try again!\n".format(diff))
+            self.display_difficulties()
+            diff = input("Difficulty: ")
+
+        return diff
+
+    def read_from_file(self,b,t):
         with open("english.txt","r") as file:
             pattern = re.compile(r"\n")
             lst = file.readlines()
             for word in range(len(lst)):
                 lst[word] = re.sub(pattern,"",lst[word])
-                if len(lst[word]) >= 3 and len(lst[word]) <= 5:
-                    self.words.append(lst[word]) 
+                if len(lst[word]) >= b and len(lst[word]) <= t:
+                    self.words.append(lst[word])
     
 
     def choose_word(self):
@@ -29,20 +50,17 @@ class Hangman():
     def ask_for_input(self):
         alphabet = string.ascii_lowercase
         inp = input("Guess: ")
-        while True:
-            if inp not in alphabet:
+        while inp not in alphabet:
                 print("{} is not supported in this game. Try again!".format(inp))
                 inp = input("Letter: ")
-            else:
-                return inp
+
+        return inp
     
     def letter_has_been_used(self, letter):
         for i in range(len(self.used_letters)):
             if self.used_letters[i] == letter:
                 return True
         return False
-
-
     
     def right_guess(self, word, dashed_word, letter):
         i = 0
@@ -55,26 +73,36 @@ class Hangman():
         if in_word:
             return True
     
-    def success(self, inp, puzzle_word):
+    def right_guess_feedback(self, inp, puzzle_word):
         print("\033[1;32mGood job! There is a(n) {}\033[0m\n".format(inp))
         print("\033[1;32mWord: {}\033[0m".format(" ".join(puzzle_word)))
     
-    def failure(self,inp, puzzle_word):
+    def wrong_guess_feedback(self,inp, puzzle_word):
         print("\033[1;31mSorry, no letter {}.\033[0m\n".format(inp))
         print("\033[1;31mWord: {}\033[0m".format(" ".join(puzzle_word)))
 
-    def won(self, dashed_word):
+    def game_over_won(self, chosen_word, dashed_word, tries):
         if "_" not in " ".join(dashed_word):
+            print("-"*30)
+            print("\n\033[1;32;4;5mYOU WON!\033[0m")
+            print("Wooo! You figured out the word '{}' in {} tries.".format(chosen_word, 11-self.tries))
             return True
+    
+    def game_over_lost(self, chosen_word):
+        print("-"*30)
+        print("\033[1;31;4;5mGAME OVER!\033[0m\n")
+        print("The word was '{}'.".format(chosen_word))
 
     def play_game(self):
+        diff = self.display_intro_set_difficulty()
+        self.read_from_file(self.difficulty[diff][1][0],self.difficulty[diff][1][1])
         chosen_word = self.choose_word()
         puzzle_word = self.dashed_word(chosen_word)
         won = False
         print("The word is: {}\n".format(" ".join(puzzle_word)))
         while self.tries > 0 and not won:
             print("-"*30)
-            print("Try: {}".format(11-self.tries))
+            print("Tries left: {}".format(self.tries))
             inp = self.ask_for_input()
             while self.letter_has_been_used(inp):
                 print("You already used letter: '{}'".format(inp))
@@ -82,17 +110,14 @@ class Hangman():
             self.used_letters.append(inp)
             print("Used letters: {}\n".format(sorted(self.used_letters)))
             if self.right_guess(chosen_word,puzzle_word,inp):
-                self.success(inp,puzzle_word)
+                self.right_guess_feedback(inp,puzzle_word)
             else:
-                self.failure(inp, puzzle_word)
+                self.wrong_guess_feedback(inp, puzzle_word)
+                self.tries -= 1
             
 
-            if self.won(puzzle_word):
-                print("Wooo! You figured out the word '{}' in {} tries.".format(chosen_word, 11-self.tries))
+            if self.game_over_won(chosen_word, puzzle_word, self.tries):
                 won = True
-            else:
-                self.tries -= 1
-
-        if self.tries == 0:
-            print("The word was '{}'.".format(chosen_word))
+            elif self.tries == 0:
+                self.game_over_lost(chosen_word)
 
