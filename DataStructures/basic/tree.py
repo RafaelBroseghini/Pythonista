2ca54da891febd63914e02e233c0a262024e6a73
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-06-16 16:18:24 -0500

    Refreshing bin trees

--- DataStructures/basic/tree.py
+++ DataStructures/basic/tree.py
@@ -1,40 +1,56 @@
 class BinaryTree(object):
-    """docstring for BinaryTree."""
-    def __init__(self, rootObj):
-        self.key = rootObj
-        self.leftChild = None
+    def __init__(self,root):
+        self.root       = root
+        self.leftChild  = None
         self.rightChild = None
-
-    def insertLeft(self, newBranch):
-        if self.leftChild is None:
-            self.leftChild = BinaryTree(newBranch)
-        else:
-            temp = BinaryTree(newBranch)
-            temp.leftChild = self.leftChild
-            self.leftChild = temp
-
-    def insertRight(self, newBranch):
-        if self.rightChild is None:
-            self.rightChild = BinaryTree(newBranch)
-        else:
-            temp = BinaryTree(newBranch)
-            temp.rightChild = self.rightChild
-            self.rightChild = temp
-
+    
+    def getRootVal(self):
+        return self.root
+    
     def getLeftChild(self):
         return self.leftChild
-
+    
     def getRightChild(self):
         return self.rightChild
 
-    def setRootVal(self, nv):
-        self.key = nv
+    def insertLeft(self, value):
+        if self.getLeftChild() is None:
+            self.leftChild = BinaryTree(value)
+        else:
+            temp = BinaryTree(value)
+            temp.leftChild = self.leftChild
+            self.leftChild = temp
+    
+    def insertRight(self, value):
+        if self.getRightChild() is None:
+            self.rightChild = BinaryTree(value)
+        else:
+            temp = BinaryTree(value)
+            temp.rightChild = self.rightChild
+            self.rightChild = temp
 
-    def getRootVal(self):
-        return self.key
 
-t = BinaryTree(5)
-t.insertLeft(9)
-t.insertLeft(10)
+def maxHeight(tree):
+    if tree is None:
+        return 0
+    
+    # print(tree.root)
+    
+    leftH = maxHeight(tree.leftChild)
+    rightH = maxHeight(tree.rightChild)
 
-print(t.getLeftChild())
+    return max(leftH, rightH) + 1
+
+def main():
+    b = BinaryTree(10)
+    b.insertLeft(9)
+    a = b.getLeftChild()
+    for i in range(100):
+        a.insertRight(i)
+            # for i in range(10):
+    #     b.insertLeft(i)
+    #     b.insertRight(i+10)
+
+    print(maxHeight(b))
+if __name__ == '__main__':
+    main()
