415f23744dd21ec458de38374213790a8ac79a52
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-06-24 17:07:14 -0500

    refactor and prettify

--- OOP/DiceGame.py
+++ OOP/DiceGame.py
@@ -1,39 +1,33 @@
 #RAFAEL BROSEGHINI
-#CS-160
 
 #!/usr/bin/env python3
 #encoding: UTF-8
 
 import random
 # Need a seed for randomization
-random.seed(42)
+# random.seed(42)
 
 class Die:
     '''A die class'''
-    def __init__(self, possible_values_):
-        self.possible_values = possible_values_
-        self._value = random.choice(possible_values_)
-        # Task 1: assign random value (out of the list of possible_values) to self._value
+    def __init__(self, possible_values):
+        self._possible_values = possible_values
+        self._value = 1
 
     def roll(self):
         '''Roll the die'''
-        self._value = random.choice(self.possible_values)
+        self._value = random.choice(self._possible_values)
         return self._value
-        # Task 2: assign random value (out of the list of possible_values) to self._value and return it
 
     @property
     def value(self):
-        ''''value' property getter'''
         return self._value
 
     @value.setter
     def value(self, _):
-        ''''value' property setter'''
         if _ >= 1 and _ <= 6:
             self._value = _
         else:
             raise ValueError('Value is not acceptable for the use of a die.')        
-        # Task 3: raise ValueError exception with a custom message
 
     @value.deleter
     def value(self):
@@ -43,28 +37,22 @@ class Die:
     def __str__(self):
         '''__str__ override'''
         return str(self._value)
-        # Task 4: return self._value as a string
 
 class Cup:
-    '''A cup class'''
-    def __init__(self, num_dice_, num_sides_=6):
-        self._dice = [Die(range(1, num_sides_ + 1)) for _ in range(num_dice_)]
+    def __init__(self, num_dice, num_sides_=6):
+        self._dice = [Die(range(1, num_sides_ + 1)) for _ in range(num_dice)]
 
     def shake(self):
-        '''Shake a cup'''
         for i in self._dice:
-            i.roll()
-        # Task 5: roll every die in self._dice list
+            print(i.roll())
 
     def remove(self, idx_):
         '''Remove a die from the cup'''
         self._dice.pop(idx_)
-        # Task 6: pop a die with index idx from the self._dice list
 
     def add(self, die_):
         '''Add a die to the cup'''
         self._dice.append(die_)
-        # Task 7: append a new die to the self._dice list
 
     def roll(self, *args):
         '''Roll the dice'''
@@ -72,51 +60,50 @@ class Cup:
             if i > 0 and i <= len(args):
                 self._dice[i-1].roll()
 
+    def total(self):
+        return sum([d._value for d in self._dice])
+
+    def __len__(self):
+        return len(self._dice)
+
     def __str__(self):
         '''__str__ override'''
         return str([i.value for i in self._dice])
-        # Task 8: return a list of dice as a string
+
+    def __getitem__(self, idx):
+        return self._dice[idx]
 
     def __iter__(self):
         '''Cup iterator'''
         return iter(self._dice)
 
+
+
+
+def game(num_dices_per_player, maxScore):
+    p1 = Cup(num_dices_per_player)
+    p2 = Cup(num_dices_per_player)
+    scoreBoard = {p1: 0, p2: 0}
+    i = 0
+    while scoreBoard[p1] < maxScore and scoreBoard[p2] < maxScore:
+        p1Roll = p1[i].roll()
+        p2Roll = p2[i].roll()
+        if p1Roll > p2Roll:
+            scoreBoard[p1] += 1
+        elif p1Roll == p2Roll:
+            scoreBoard[p1] += 0
+            scoreBoard[p2] += 0
+        else:
+            scoreBoard[p2] += 1
+        i += 1
+        if i == num_dices_per_player:
+            i = 0
+    return "The winner is: {}".format(scoreBoard)
+
 def main():
     '''Entry point'''
     print("Let's play a game!")
-    print("Let's create a die")
-    my_die = Die(range(1, 7))
-    print("Rolling die 10 times")
-    for i in range(10):
-        my_die.roll()
-        print("{:2}) {:>2}".format(i+1, str(my_die)))
-    print("The last roll is {}".format(my_die.value))
-    print("Trying to assign a value to a die")
-    try:
-        my_die.value = 7
-    except ValueError as die_exception:
-        print(die_exception)
-    print("The last roll is (still) {}".format(my_die.value))
-    print("Making a cup of 5 dice")
-    my_cup = Cup(5)
-    print(my_cup)
-    print("Re-rolling dice 1, 2, and 3")
-    my_cup.roll(1, 2, 3)
-    print(my_cup)
-    print("Re-rolling dice 2, 4, and 6")
-    my_cup.roll(1, 2, 3)
-    print(my_cup)
-    print("Shaking and rolling")
-    for _ in range(3):
-        my_cup.shake()
-        print(my_cup)
-    print("Using the iterator to print dice values")
-    for a_die in my_cup:
-        print(a_die)
-    print("Over and out")
+    print(game(10,100))
 
 if __name__ == "__main__":
     main()
-
-# Task 9: the code is PEP8 compliant
-# Task 10: the program is working
