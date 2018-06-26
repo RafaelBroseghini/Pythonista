332f96fc34824a6eaeb8fca572ad540f7c851a0d
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-06-25 22:02:28 -0500

    object player takes cup and plays

--- OOP/DiceGame/main.py
+++ OOP/DiceGame/main.py
@@ -4,31 +4,43 @@
 #encoding: UTF-8
 
 from cup import Cup
+from player import Player
 
-def game(num_dices_per_player, maxScore):
-    p1 = Cup(num_dices_per_player)
-    p2 = Cup(num_dices_per_player)
-    scoreBoard = {p1: 0, p2: 0}
+def get_winner(scoreBoard, maxScore):
+    for player in scoreBoard:
+        if scoreBoard[player] == maxScore:
+            return player
+
+def game(p1, p2, num_dices_per_player, maxScore):
+    scoreBoard = {p1.name: 0, p2.name: 0}
     i = 0
-    while scoreBoard[p1] < maxScore and scoreBoard[p2] < maxScore:
-        p1Roll = p1[i].roll()
-        p2Roll = p2[i].roll()
+    while scoreBoard[p1.name] < maxScore and scoreBoard[p2.name] < maxScore:
+        p1Roll = p1.cup[i].roll()
+        p2Roll = p2.cup[i].roll()
         if p1Roll > p2Roll:
-            scoreBoard[p1] += 1
+            scoreBoard[p1.name] += 1
         elif p1Roll == p2Roll:
-            scoreBoard[p1] += 0
-            scoreBoard[p2] += 0
+            scoreBoard[p1.name] += 0
+            scoreBoard[p2.name] += 0
         else:
-            scoreBoard[p2] += 1
+            scoreBoard[p2.name] += 1
         i += 1
         if i == num_dices_per_player:
             i = 0
-    return "The winner is: {}".format(scoreBoard)
+    
+    winner = get_winner(scoreBoard, maxScore)
+    return "....And the winner is: {} !!!".format(winner)
 
 def main():
     '''Entry point'''
-    print("Let's play a game!")
-    print(game(10,100))
+    print("Let's play a game!\n")
+    dices = int(input("How many die? "))
+    points = int(input("How many points? "))
+
+    p1 = Player("Rafa",Cup(dices))
+    p2 = Player("Mo",Cup(dices))
+    
+    print(game(p1,p2,dices,points))
 
 if __name__ == "__main__":
     main()
