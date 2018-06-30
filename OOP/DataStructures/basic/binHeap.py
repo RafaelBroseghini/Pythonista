9b3f67d1ee7be8b4fa098e0743062ec602869178
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-06-29 22:56:29 -0500

    min Heap, studies

--- OOP/DataStructures/basic/binHeap.py
+++ OOP/DataStructures/basic/binHeap.py
@@ -1,69 +1,74 @@
+import unittest
+
 class BinHeap(object):
     def __init__(self):
-        self.heaplist = [0]
+        self.heapList = [0]
         self.currentSize = 0
 
-    def percUp(self, i):
-        while i // 2 > 0:
-            if self.heaplist[i] < self.heaplist[i // 2]:
-                temp = self.heaplist[i // 2]
-
-                self.heaplist[i//2] = self.heaplist[i]
-                self.heaplist[i] = temp
-            i = i // 2
-
-    def insert(self, k):
-        self.heaplist.append(k)
+    def insert(self, key):
+        # Simple append. Increase size. Percolate that item up.
+        self.heapList.append(key)
         self.currentSize += 1
-        self.percUp(self.currentSize)
+        i = self.currentSize
+        self.percUp(i)
 
+    def percUp(self, i):
+        # We may need to percolate the item all the way.
+        # Please note that parents are at index//2.
+        # Compare current index with index//2 and perform swap if necessary.
+        while i // 2 > 0:
+            if self.heapList[i] < self.heapList[i//2]:
+                self.heapList[i], self.heapList[i//2] = self.heapList[i//2],self.heapList[i]
+            i = i // 2
+    
     def percDown(self, i):
+        # We only need to go until len(heapList)//2 since at that point
+        # it is the level of nodes before last.
         while (i*2) <= self.currentSize:
-
             mc = self.minChild(i)
-            print("The min child is at index: ", mc)
-            if self.heaplist[i] > self.heaplist[mc]:
-                print("Swapping {} with {}".format(self.heaplist[i], self.heaplist[mc]))
-                temp = self.heaplist[i]
-
-                self.heaplist[i] = self.heaplist[mc]
-                self.heaplist[mc] = temp
-                print(self.heaplist)
-
-            i = mc
+            if self.heapList[i] > self.heapList[mc]:
+                self.heapList[i], self.heapList[mc] = self.heapList[mc], self.heapList[i]
+            i += 1
 
     def minChild(self, i):
         if i * 2 + 1 > self.currentSize:
-            return i * 2
+            return i * 2 + 1
         else:
-            if self.heaplist[i*2] < self.heaplist[i*2+1]:
+            # Comparing two children and return index of smaller one.
+            if self.heapList[i*2] < self.heapList[i*2+1]:
                 return i * 2
             else:
                 return i * 2 + 1
 
     def delMin(self):
-        retval = self.heaplist[1]
-        self.heaplist[1] = self.heaplist[self.currentSize]
+        # Reassing min item to top.
+        # Decrease size.
+        # Percolate that item down.
+        # pop repeated instance from end of heapList.
+        self.heapList[1] = self.heapList[self.currentSize]  
         self.currentSize -= 1
-        self.heaplist.pop()
         self.percDown(1)
-        return retval
+        self.heapList.pop()
 
-    def buildHeap(self, lst):
-        i = len(lst) // 2
-        self.currentSize = len(lst)
-        self.heaplist = [0] + lst[:]
-
-        print("Initial list {}".format(self.heaplist))
-        while (i != 0):
-            print("The percolating is starting from index: {}, which is number {}.".format(i,self.heaplist[i]) )
+    def buildHeap(self, array):
+        i = len(array) // 2
+        self.currentSize = len(array)
+        self.heapList = [0] + array[:]
+        while i > 0:
             self.percDown(i)
-            print(self.heaplist)
             i -= 1
+            
 
-b = BinHeap()
-
-lst = [4,3,2,1]
-
-
-b.buildHeap(lst)
+class Tester(unittest.TestCase):
+    def test_BinHeap(self):
+        b = BinHeap()
+        b.insert(8)
+        b.insert(1)
+        b.insert(7)
+        b.insert(6)
+        b.insert(5)
+        b.insert(3)
+        self.assertEqual(b.heapList, [0,1,5,3,8,6,7])
+    
+if __name__ == '__main__':
+    unittest.main()
