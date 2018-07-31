75dcb04645d77a11dbeb08f430e01fee44758866
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-07-30 22:20:52 -0500

    automate more

---
+++ Automate this Please!/Delete test files/delete_test_files.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+"""
+This script gets rid of all the 'test' files that
+you may have created and forgot about it.
+
+For people starting out in programming it
+is common to create 'test.*' files, for quick
+dirty code. Maybe just checking if that
+list comprehension works, maybe trying a new
+control flow etc.
+"""
+
+import os
+import re
+import sys
+
+__author__ = "Rafael Broseghini"
+__credits__ = ["Rafael Broseghini"]
+__version__ = "1.0.1"
+__maintainer__ = "Rafael Broseghini"
+__email__ = "rafaellopesbroseghini@gmail.com"
+__status__ = "Prototype"
+
+
+
+class TestFileEraser(object):
+    PATH = os.getcwd()
+
+    def __init__(self):
+        self.all_files = os.listdir(self.PATH)
+
+
+    def get_test_files(self, extension=sys.argv[1]):
+        """
+        Returns array with files starting with 'test'.
+        If extension is specified, only files with that
+        extension will be matched.
+        """
+
+        result = []
+        pattern = "^test"+extension
+        for file in self.all_files:
+            if re.search(pattern,file) and not os.path.isdir(file):
+                result.append(file)
+        
+        return result
+
+
+    def delete_test_files(self, files_list=[]):
+        """
+        Deletes all 'test' files passed in files_list
+        parameter.
+        """
+        for file in files_list:
+            os.remove(file)
+
+
+
+def main():
+    t = TestFileEraser()
+    test_files = t.get_test_files()
+    t.delete_test_files(test_files)
+
+if __name__ == '__main__':
+    main()
+
+
--- Automate this Please!/File Concatenation/file_concatenator.py
+++ Automate this Please!/File Concatenation/file_concatenator.py
@@ -1,25 +1,58 @@
+#!/usr/bin/env python
+
+"""
+Concatenates content of all files with specified
+extension into one output file.
+"""
+
 import os
+import sys
 import re
+
+__author__ = "Rafael Broseghini"
+__credits__ = ["Rafael Broseghini"]
+__version__ = "1.0.1"
+__maintainer__ = "Rafael Broseghini"
+__email__ = "rafaellopesbroseghini@gmail.com"
+__status__ = "Prototype"
+
 
 class FileConcatenator(object):
     PATH = os.getcwd()
     
     def __init__(self):
+        """
+        Initialize class member to a list with all
+        files and directories within current path.
+        """
         self.file_list = os.listdir(self.PATH)
-        # self.files_with_extension = []
 
-    def get_files_with_extension(self, extension=None):
-        if extension == None:
+    def get_files_with_extension(self, extension=sys.argv[1]):
+        """
+        Gets all files from current path with specified
+        extension. If no extension is provided, returns
+        an error.
+        """
+        if extension == "":
             raise EnvironmentError("No extension provided!")
 
         result = []
         for idx, file in enumerate(self.file_list):
             if re.search(extension+"$",file):
                 result.append(file)
+        
+        if len(result) == 0:
+            raise Exception("No {} files found.".format(extension))
+
         return result
 
-    def write_to_master_file(self, filename=None, all_files=[], separator=""):
-        if filename == None:
+    def write_to_master_file(self, all_files=[], filename=sys.argv[2], separator=sys.argv[3]):
+        """
+        Writes content of all found files with extension
+        to a single source name passed as command line argument
+        by the user.
+        """
+        if filename == "":
             raise EnvironmentError("No filename provided!")
 
         first_file = all_files[0]
@@ -28,6 +61,7 @@ class FileConcatenator(object):
             with open(first_file, "r+") as initial_write:
                 for line in initial_write:
                     master.write(line)
+
                 if len(all_files) > 1:
                     for i in range(1, len(all_files)):
                         master.write(separator)
@@ -38,8 +72,8 @@ class FileConcatenator(object):
 
 def main():
     a = FileConcatenator()
-    all_files_txt = a.get_files_with_extension(".txt") 
-    a.write_to_master_file("theConcatenation.txt", all_files_txt,"\n")
+    all_found_files = a.get_files_with_extension() 
+    a.write_to_master_file(all_found_files)
 
 if __name__ == '__main__':
     main()
