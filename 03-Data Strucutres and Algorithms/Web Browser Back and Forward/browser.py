8eeb4cfbeead5a72ea0925c3c5937814b316172c
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-08-01 16:02:15 -0500

    Create browser.py

---
+++ 03-Data Strucutres and Algorithms/Web Browser Back and Forward/browser.py
@@ -0,0 +1,36 @@
+from pythonds.basic.stack import Stack
+
+__author__ = "Rafael Broseghini"
+__credits__ = ["Rafael Broseghini"]
+__version__ = "1.0.1"
+__maintainer__ = "Rafael Broseghini"
+__email__ = "rafaellopesbroseghini@gmail.com"
+__status__ = "Prototype"
+
+class Browser(object):
+  def __init__(self, current=None):
+    self._current_url  = current
+    self._backward = Stack()
+    self._forward  = Stack()
+
+  @property
+  def backward(self):
+    return self._backward
+  
+  @property
+  def forward(self):
+    return self._forward
+
+  @property
+  def current_url(self):
+    return self._current_url
+
+  @current_url.setter
+  def current_url(self, new_url):
+    self._current_url = new_url
+
+  def check_back_and_forth(self, url):
+    if url == "back" and self.backward.isEmpty():
+      raise Exception("Can't go back to nowhere.")
+    elif url == "forward" and self.forward.isEmpty():
+      raise Exception("Can't go forward to nowhere.")
