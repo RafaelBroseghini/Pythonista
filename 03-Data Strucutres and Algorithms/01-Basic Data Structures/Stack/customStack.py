8ac81893a8c8fb1ae61233a8df36194f6c63156c
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-08-04 22:32:24 -0500

    reverse

--- 03-Data Strucutres and Algorithms/01-Basic Data Structures/Stack/customStack.py
+++ 03-Data Strucutres and Algorithms/01-Basic Data Structures/Stack/customStack.py
@@ -43,15 +43,41 @@ class Stack(object):
     def size(self):
         return len(self.items)
 
+# Reverse the current stack using a temp list (acts as queue)
+def reverse(s1):
+    temp = []
 
+    while not s1.isEmpty():
+        temp.insert(0, s1.pop())
+    
+    while not len(temp) == 0:
+        s1.push(temp.pop())
+
+# Reverse a given stack using two additional stacks.
+def reverse_using_two_stacks(s1):
+    temp1 = Stack()
+    temp2 = Stack()
+
+    while not s1.isEmpty():
+        temp1.push(s1.pop())
+    
+    while not temp1.isEmpty():
+        temp2.push(temp1.pop())
+
+    while not temp2.isEmpty():
+        s1.push(temp2.pop())
+        
+
+def push_all(s1, stop):
+    for i in range(1, 11):
+        s1.push(i)
 
 def main():
     s1 = Stack()
     s2 = Stack()
     s3 = []
 
-    for i in range(1, 11):
-        s1.push(i)
+    push_all(s1, 11)
 
     print("Stack 1 before transfer:", s1.items)
     print("Stack 2 before transfer:", s2.items)
@@ -60,8 +86,7 @@ def main():
     print("Stack 1 after transfer: ", s1.items)
     print("Stack 2 after transfer: ", s2.items)
 
-    for i in range(1, 11):
-        s1.push(i)
+    push_all(s1, 11)
 
     print("Stack 1 before clearing:", s1.items)
 
@@ -69,8 +94,23 @@ def main():
 
     print("Stack 1 after clearing:", s1.items)
 
+    push_all(s1, 11)
 
+    print("Stack 1 before reversing:", s1.items)
 
+    reverse(s1)
+
+    print("Stack 1 after reversing:", s1.items)
+
+    # Here we reverse the stack again to
+    # put items back in order.
+    reverse(s1)
+
+    print("Stack 1 before reversing using two addtional stacks:", s1.items)
+    
+    reverse_using_two_stacks(s1)
+
+    print("Stack 1 after reversing using two addtional stacks:", s1.items)
 
 if __name__ == '__main__':
     main()
