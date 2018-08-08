f12a3922876c6d09c5abf03a5792312d01802635
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-08-07 22:47:39 -0500

    one more

---
+++ 03-Data Strucutres and Algorithms/Recursion/cumulativeSum.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+Given a number calculate
+it's cumulative sum. From n to 0.
+"""
+
+def cumulative_sum(num):
+    if num == 0:
+        return 0
+    else:
+        return num + cumulative_sum(num-1)
+
+
+
+assert cumulative_sum(10) == 55
