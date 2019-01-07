5496fd763d29559c76549929498a4e80e8294dde
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2019-01-07 12:50:59 -0600

    reordered code

--- 03-Data Strucutres and Algorithms/02-Algorithms/Heuristic Searches/best_first_search.py
+++ 03-Data Strucutres and Algorithms/02-Algorithms/Heuristic Searches/best_first_search.py
@@ -11,8 +11,6 @@ g = [
   [0,1,1,1,1]
 ]
 
-drawing = [["0" for i in range(len(g[0]))] for i in range(len(g))]
-
 class Item(object):
   def __init__(self, coord, distance):
         self.coord = coord
@@ -31,11 +29,6 @@ class Item(object):
 def mhd(current, goal):
     return abs(current[0] - goal[0]) + abs(current[1] - goal[1])
 
-def append_neighbors(x: int, y: int, graph: list, array:list):
-    if x >= 0 and x <= len(graph)-1 and y >= 0 and y <= len(graph[0])-1:
-        if graph[x][y]!= 1:
-            array.append([x, y])
-
 def get_neighbors(graph: list, coord: list):
     neighbors = []
     append_neighbors(coord[0]-1, coord[1], graph, neighbors)
@@ -43,6 +36,27 @@ def get_neighbors(graph: list, coord: list):
     append_neighbors(coord[0], coord[1]-1, graph, neighbors)
     append_neighbors(coord[0], coord[1]+1, graph, neighbors)
     return neighbors
+
+def append_neighbors(x: int, y: int, graph: list, array:list):
+    if x >= 0 and x <= len(graph)-1 and y >= 0 and y <= len(graph[0])-1:
+        if graph[x][y]!= 1:
+            array.append([x, y])
+
+def best_first_search(graph, source, goal):
+    visited, unvisited, path = set(), [], []
+    heapq.heappush(unvisited,Item(source, 0))
+    while len(unvisited) > 0:
+        curr = heapq.heappop(unvisited)
+        path.append(curr.coord)
+        if curr.coord == goal:
+            return path
+        visited.add(tuple(curr.coord))
+        neighbors = get_neighbors(graph, curr.coord)
+        for n in neighbors:
+            if tuple(n) not in visited:
+                dis = mhd(n, goal)
+                heapq.heappush(unvisited, Item(n, dis))
+    return []
 
 def draw_path(x: int, y: int, source: list, goal: list, path: list):
     drawing = [[" " for i in range(x)] for j in range(y)]
@@ -60,22 +74,6 @@ def draw_path(x: int, y: int, source: list, goal: list, path: list):
 
     for line in drawing:
         print(" ".join(line))
-
-def best_first_search(graph, source, goal):
-    visited, unvisited, path = set(), [], []
-    heapq.heappush(unvisited,Item(source, 0))
-    while len(unvisited) > 0:
-        curr = heapq.heappop(unvisited)
-        path.append(curr.coord)
-        if curr.coord == goal:
-            return path
-        visited.add(tuple(curr.coord))
-        neighbors = get_neighbors(graph, curr.coord)
-        for n in neighbors:
-            if tuple(n) not in visited:
-                dis = mhd(n, goal)
-                heapq.heappush(unvisited, Item(n, dis))
-    return []
 
 def main():
     source, target = [0,0], [6,0]
