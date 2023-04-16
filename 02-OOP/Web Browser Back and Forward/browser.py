bf84d7fbf2262615e20fb65983013b35775f3c59
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2023-04-16 18:17:29 -0500

    improve code

--- 02-OOP/Web Browser Back and Forward/browser.py
+++ 02-OOP/Web Browser Back and Forward/browser.py
@@ -12,40 +12,48 @@ __maintainer__ = "Rafael Broseghini"
 __status__ = "Prototype"
 
 
-class Browser(object):
+"""
+Basic implementation of a Web Browser
+'back' and 'forward' button operations.
+"""
+
+
+class Browser:
     def __init__(self, current=None):
         self._current_url = current
         self._backward = Stack()
         self._forward = Stack()
 
     @property
-    def backward(self) -> Stack:
+    def backward(self):
         return self._backward
 
     @property
-    def forward(self) -> Stack:
+    def forward(self):
         return self._forward
 
     @property
-    def current_url(self) -> str:
+    def current_url(self):
         return self._current_url
 
     @current_url.setter
-    def current_url(self, new_url: str) -> str:
+    def current_url(self, new_url):
         self._current_url = new_url
 
-    def check_back_and_forth(self, url: str) -> None:
+    def check_back_and_forth(self, url):
         if url == "back" and self.backward.isEmpty():
             raise Exception("Can't go back to nowhere!")
         elif url == "forward" and self.forward.isEmpty():
             raise Exception("Can't go forward to nowhere.")
 
-    def go_back(self) -> None:
-        self.forward.push(self.current_url)
-        previous = self.backward.pop()
-        self.current_url = previous
+    def go_back(self):
+        if not self.backward.isEmpty():
+            self.forward.push(self.current_url)
+            previous = self.backward.pop()
+            self.current_url = previous
 
-    def go_forward(self) -> None:
-        self.backward.push(self.current_url)
-        forward = self.forward.pop()
-        self.current_url = forward
+    def go_forward(self):
+        if not self.forward.isEmpty():
+            self.backward.push(self.current_url)
+            forward = self.forward.pop()
+            self.current_url = forward
--- 02-OOP/Web Browser Back and Forward/surfing_the_web.py
+++ 02-OOP/Web Browser Back and Forward/surfing_the_web.py
@@ -5,7 +5,7 @@ the 'back' and 'forward' functionality
 in a Web Browser work.
 
 Using two stacks, one is able to
-re create this functionality by
+re-create this functionality by
 popping and pushing from one stack
 into the other and keeping a reference
 to the current_url.
@@ -14,24 +14,19 @@ from browser import Browser
 
 
 def surf_the_web(browser):
-    inp = input("Site you want to visit: ")
-    browser.current_url = inp
+    inp = input("Site you want to visit (type 'None' to quit): ")
 
     while inp != "None":
-        current = inp
-
-        browser.check_back_and_forth(inp)
-
         if inp == "back":
             browser.go_back()
         elif inp == "forward":
             browser.go_forward()
         else:
-            if current != browser.current_url:
+            if inp != browser.current_url:
                 browser.backward.push(browser.current_url)
-                browser.current_url = current
-
-        inp = input("Site you want to visit: ")
+                browser.current_url = inp
+        print(f"Current site: {browser.current_url}")
+        inp = input("Site you want to visit (type 'None' to quit): ")
 
 
 def main():
